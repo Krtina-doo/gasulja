7491d8958ecf872a61236965a62edca0dfc910ce
alendahalic <idega6s@gmail.com>
2021-04-29 00:09:15 +0200

    wewewerggfrcv

--- js/main.js
+++ js/main.js
@@ -5,8 +5,12 @@ $(document).ready(function(){
     $(window).bind('scroll', function () {
         if ($(window).scrollTop() > 24) {
             $('#nav-big').addClass('navscroll');
+            $('#gg-menu').addClass('menuscroll');
+            $('#soft-menu').addClass('menuscroll');
         } else {
             $('#nav-big').removeClass('navscroll');
+            $('#gg-menu').removeClass('menuscroll');
+            $('#soft-menu').removeClass('menuscroll');
         }
     });
     
@@ -19,12 +23,12 @@ $(document).ready(function(){
         console.log(poz.left);
         switch (poz.left){
             case 0:
-                $('.roll').css('left', '-=250');
+                $('.roll').css('left', '-=350');
                 tipkaNazad.show();
                 $(this).show();
             break;
-            case -250:
-                $('.roll').css('left', '-=250');
+            case -350:
+                $('.roll').css('left', '-=350');
                 $(this).hide();
             break;
         }
@@ -33,15 +37,15 @@ $(document).ready(function(){
    tipkaNazad.click(function(){
         var poz = $('.roll').position();
         switch (poz.left){
-            case -250:
-                $('.roll').css('left', '+=250');
+            case -350:
+                $('.roll').css('left', '+=350');
                 tipkaNazad.hide();
                 if(tipkaNapr.css('display')=='none'){
                     tipkaNapr.show();
                 }
             break;
-            case -500:
-                $('.roll').css('left', '+=250');
+            case -700:
+                $('.roll').css('left', '+=350');
             break;
         }
     });
